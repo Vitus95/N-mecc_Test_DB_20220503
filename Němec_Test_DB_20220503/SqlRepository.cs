21afc8ee15893824884ffa70c167c71763c9bbac
Vítek Němec <Nemec.Vitek@stud.vassboskovice.cz>
2022-05-31 12:14:02 +0200

    Přidejte soubory projektu.

---
+++ Němec_Test_DB_20220503/SqlRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Němec_Test_DB_20220503
+{
+    public class SqlRepository
+    {
+        private string connString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=NěmecFaktury;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    }
+}
+
+
+
+
